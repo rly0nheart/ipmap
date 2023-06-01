f27d2b66e3d1a44efd31df355a3539fa3c88e34f
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-06-02 00:26:31 +0200

    Update utils.py

--- ipmap/utils.py
+++ ipmap/utils.py
@@ -1,18 +1,19 @@
 import os
-import argparse
 import subprocess
 from datetime import datetime
 
 
-def format_map_name():
+def format_map_name(defined_name) -> str:
     """
     Formats the output map name
+    :param defined_name: user-defined map name
+    :return: formatted map name
     """
     dt_now = datetime.now()
     if os.name == "nt":
-        map_name = dt_now.strftime("{}_%d-%m-%Y %I-%M-%S%p")
+        map_name = dt_now.strftime(f"{defined_name}_%d-%m-%Y %I-%M-%S%p")
     else:
-        map_name = dt_now.strftime("{}_%d-%m-%Y %I:%M:%S%p")
+        map_name = dt_now.strftime(f"{defined_name}_%d-%m-%Y %I:%M:%S%p")
         
     return map_name
 
@@ -35,36 +36,3 @@ def clear_screen() -> None:
     :return: None
     """
     subprocess.call("cmd.exe /c cls" if os.name == "nt" else "clear")
-
-
-def usage():
-    return """
-    Geolocate IP Address(es) (with an interactive map)
-    --------------------------------------------------
-    ipmap map --ip <an_ip_or_a_file_containing_ip_addresses>
-    
-    Open Google Earth with the given coordinates
-    --------------------------------------------
-    ipmap earth --coordinates <latitude> <longitude>
-         
-    Lookup IP Address(es) (same as map but without an interactive map)
-    ------------------------------------------------------------------
-    ipmap lookup --ip <an_ip_or_a_file_containing_ip_addresses>
-    
-    
-modes:
-    map - creates an interactive map and pin points the locations of the specified ip address(es) on it.
-    earth - opens google earth with the specified coordinates.
-    lookup - looks up the specified ip address(es)' information.
-    """
-
-
-# Create parser
-def create_parser():
-    parser = argparse.ArgumentParser(description="IP.MAPPER — by Richard Mwewa (https://about.me/rly0nheart)",
-                                     usage=usage())
-    parser.add_argument("mode", help="init mode", choices=["earth", "lookup", "map", "maps"])
-    parser.add_argument("-i", "--ip", help="an ip address or a file containing ip addresses")
-    parser.add_argument("-o", "--output", help="map output name", default="ipmap")
-    parser.add_argument("-c", "--coordinates", help="space separated latitude and longitude", nargs=2)
-    return parser
