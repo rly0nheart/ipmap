ad40c16317b7a6cf688143da10b12d58e0a9fb57
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-06-05 21:12:16 +0200

    Update main.py

--- ipmap/main.py
+++ ipmap/main.py
@@ -1,6 +1,7 @@
+import time
 from ipmap.ipmap import *
-from ipmap.config import format_map_name, create_parser
 from ipmap.utils import path_finder, clear_screen, check_updates
+from ipmap.config import format_map_name, create_parser, settings, Version
 
 
 # Parse command line arguments
@@ -9,8 +10,9 @@ args = parser.parse_args()
 
 
 def run():
-    path_finder(["maps"])
     clear_screen()
+    xprint(f"Starting {settings()['program']['name']} v{Version().full_version()} at {time.asctime()}")
+    path_finder(["maps"])
     check_updates()
     try:
         if args.mode == "earth":
@@ -18,13 +20,13 @@ def run():
         elif args.mode == "lookup":
             get_ip_data(args.ip)
         elif args.mode == "map":
-            ip_coordinates = get_ip_data(args.ip)
-            generated_map = create_map(ip_coordinates, os.path.join("maps", format_map_name(args.output)))
+            generated_map = create_map(get_ip_data(args.ip), os.path.join("maps", format_map_name(args.output)))
+            xprint(f"Opening map: {generated_map}")
             webbrowser.open(generated_map)
     except KeyboardInterrupt:
-        xprint(f"\n[{colour.YELLOW}!{colour.RESET}] User interruption detected.")
+        xprint(f"\nUser interruption detected ({colour.YELLOW}Ctrl+C{colour.RESET}).")
     except FileNotFoundError as file_not_found_error:
-        xprint(f"[{colour.YELLOW}!{colour.RESET}] File Not Found: {colour.YELLOW}{file_not_found_error}{colour.RESET}")
+        xprint(f"File Not Found: {colour.YELLOW}{file_not_found_error}{colour.RESET}")
     except Exception as error:
-        xprint(f"[{colour.RESET}X{colour.RESET}] Error: {colour.RESET}{error}{colour.RESET}")
+        xprint(f"Error: {colour.RESET}{error}{colour.RESET}")
 
